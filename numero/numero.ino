4d33596360fd0c9eb2e43ed0eacb5bc769f06513
BernatCamps <bernatcampsmc@gmail.com>
2016-11-24 21:37:56 +0100

    First Upload
    Upload of the projects until 24/11/2016

---
+++ numero/numero.ino
@@ -0,0 +1,100 @@
+void setup() {
+  for(int i= 3; i<11; i++){
+    pinMode(i, OUTPUT);
+   
+  }
+    for(int t= 3; t<11; t++){
+    pinMode(t, HIGH);
+    }
+    
+       delay(5000);
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+ for(int z= 3; z<11; z++){
+    digitalWrite(z, LOW);
+    } //9
+digitalWrite(3, HIGH);
+digitalWrite(4, HIGH);
+digitalWrite(5, HIGH);
+digitalWrite(8, HIGH);
+digitalWrite(9, HIGH);
+delay(1000);
+ for(int z= 3; z<11; z++){
+    digitalWrite(z, LOW);
+    } //8
+    digitalWrite(3, HIGH);
+    digitalWrite(4, HIGH);
+    digitalWrite(5, HIGH);
+    digitalWrite(6, HIGH);
+    digitalWrite(7, HIGH);
+    digitalWrite(8, HIGH);
+    digitalWrite(9, HIGH);
+    digitalWrite(10, HIGH);
+    delay(1000);
+     for(int z= 3; z<11; z++){
+    digitalWrite(z, LOW);}
+    
+    //7
+    digitalWrite(3, HIGH);
+    digitalWrite(4, HIGH);
+    digitalWrite(5, HIGH);
+    delay(1000);
+   
+    for(int z= 3; z<11; z++){
+    digitalWrite(z, LOW);}
+    //6
+     digitalWrite(3, HIGH);
+     digitalWrite(5, HIGH);
+    digitalWrite(6, HIGH);
+    digitalWrite(7, HIGH);
+    digitalWrite(8, HIGH);
+    digitalWrite(9, HIGH);
+    digitalWrite(10, HIGH);
+    delay(1000);
+     for(int z= 3; z<11; z++){
+    digitalWrite(z, LOW);}
+    //5
+    digitalWrite(3, HIGH);
+digitalWrite(5, HIGH);
+    digitalWrite(6, HIGH);
+     digitalWrite(8, HIGH);
+    digitalWrite(9, HIGH);
+    delay(1000);
+    for(int z= 3; z<11; z++){
+    digitalWrite(z, LOW);}
+    //4
+     digitalWrite(8, HIGH);
+    digitalWrite(9, HIGH);
+     digitalWrite(4, HIGH);
+    digitalWrite(5, HIGH);
+    delay(1000);
+     for(int z= 3; z<11; z++){
+    digitalWrite(z, LOW);}
+    //3
+     digitalWrite(3, HIGH);
+    digitalWrite(4, HIGH);
+    digitalWrite(5, HIGH);
+    digitalWrite(6, HIGH);
+    digitalWrite(9, HIGH);
+    delay(1000);
+     for(int z= 3; z<11; z++){
+    digitalWrite(z, LOW);}
+    //2
+  digitalWrite(3, HIGH);
+  digitalWrite(4, HIGH);
+  digitalWrite(6, HIGH);
+  digitalWrite(9, HIGH);
+  digitalWrite(7,HIGH);
+  delay(1000);
+  for(int z= 3; z<11; z++){
+    digitalWrite(z, LOW);
+    }
+    //1
+    digitalWrite(4, HIGH);
+    digitalWrite(5, HIGH);
+    delay(1000);
+         }
---
+++ numero_4_xifres/numero_4_xifres.ino
@@ -0,0 +1,101 @@
+
+void setup() {
+  for(int i= 2; i<13; i++){
+    pinMode(i, OUTPUT);
+   
+  }
+    for(int t= 2; t<13; t++){
+    pinMode(t, HIGH);
+    }
+    
+       delay(5000);
+}
+void loop() {
+  // put your main code here, to run repeatedly:
+ for(int z= 2; z<13; z++){
+    digitalWrite(z, LOW);
+    } //9
+digitalWrite(3, HIGH);
+digitalWrite(4, HIGH);
+digitalWrite(5, HIGH);
+digitalWrite(8, HIGH);
+digitalWrite(9, HIGH);
+delay(1000);
+ for(int z= 3; z<11; z++){
+    digitalWrite(z, LOW);
+    } //8
+    digitalWrite(3, HIGH);
+    digitalWrite(4, HIGH);
+    digitalWrite(5, HIGH);
+    digitalWrite(6, HIGH);
+    digitalWrite(7, HIGH);
+    digitalWrite(8, HIGH);
+    digitalWrite(9, HIGH);
+    digitalWrite(10, HIGH);
+    delay(1000);
+     for(int z= 3; z<11; z++){
+    digitalWrite(z, LOW);}
+    
+    //7
+    digitalWrite(3, HIGH);
+    digitalWrite(4, HIGH);
+    digitalWrite(5, HIGH);
+    delay(1000);
+   
+    for(int z= 3; z<11; z++){
+    digitalWrite(z, LOW);}
+    //6
+     digitalWrite(3, HIGH);
+     digitalWrite(5, HIGH);
+    digitalWrite(6, HIGH);
+    digitalWrite(7, HIGH);
+    digitalWrite(8, HIGH);
+    digitalWrite(9, HIGH);
+    digitalWrite(10, HIGH);
+    delay(1000);
+     for(int z= 3; z<11; z++){
+    digitalWrite(z, LOW);}
+    //5
+    digitalWrite(3, HIGH);
+digitalWrite(5, HIGH);
+    digitalWrite(6, HIGH);
+     digitalWrite(8, HIGH);
+    digitalWrite(9, HIGH);
+    delay(1000);
+    for(int z= 3; z<11; z++){
+    digitalWrite(z, LOW);}
+    //4
+     digitalWrite(8, HIGH);
+    digitalWrite(9, HIGH);
+     digitalWrite(4, HIGH);
+    digitalWrite(5, HIGH);
+    delay(1000);
+     for(int z= 3; z<11; z++){
+    digitalWrite(z, LOW);}
+    //3
+     digitalWrite(3, HIGH);
+    digitalWrite(4, HIGH);
+    digitalWrite(5, HIGH);
+    digitalWrite(6, HIGH);
+    digitalWrite(9, HIGH);
+    delay(1000);
+     for(int z= 3; z<11; z++){
+    digitalWrite(z, LOW);}
+    //2
+  digitalWrite(3, HIGH);
+  digitalWrite(4, HIGH);
+  digitalWrite(6, HIGH);
+  digitalWrite(9, HIGH);
+  digitalWrite(7,HIGH);
+  delay(1000);
+  for(int z= 3; z<11; z++){
+    digitalWrite(z, LOW);
+    }
+    //1
+    digitalWrite(4, HIGH);
+    digitalWrite(5, HIGH);
+    delay(1000);
+         }
+  // put your main code here, to run repeatedly:
+
+
